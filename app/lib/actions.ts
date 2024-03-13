8767add43935c4a3fbdf99df61985fb7f24917b8
Daniel Rocha <ideveloper.fstack@gmail.com>
2024-03-13 19:36:44 -0300

    Autenticação

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -71,7 +73,7 @@ export async function deleteInvoice(id: string) {
     message?: string | null;
   };
   
-  /*
+  
   export async function createInvoice(prevState: State, formData: FormData) {
     const validatedFields = CreateInvoice.safeParse({
       customerId: formData.get('customerId'),
@@ -103,8 +105,8 @@ export async function deleteInvoice(id: string) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   }
-  */
-
+  
+/*
   export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
       customerId: formData.get('customerId'),
@@ -128,7 +130,7 @@ export async function deleteInvoice(id: string) {
    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
-  }
+  }*/
 
   /*
   export async function updateInvoice(id: string, formData: FormData) {
@@ -198,5 +200,24 @@ export async function deleteInvoice(id: string) {
       return { message: 'Deleted Invoice.' };
     } catch (error) {
       return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
+  }
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
     }
   }
